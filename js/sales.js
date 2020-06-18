619bd427049ff56e5401cbbe6bdd37d76b43ae26
AggieWB <usmaggie23@gmail.com>
2020-06-18 08:45:15 -0700

    Move cookieLocations to have global access, and declare the
    function randomCustomersPerHour() outside each object literal to be called with forEach() on cookieLocations
    Update createStoreLocationHTMLSections() to pass in the global cookieLocations
    Change openHours to be clearer

--- js/sales.js
+++ js/sales.js
@@ -3,9 +3,6 @@ var firstPike = {
     minHourlyCustomers: 23,
     maxHourlyCustomers: 65,
     averageCookiesPerCustomer: 6.3,
-    randomCustomersPerHour: function() {
-        return Math.ceil(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;
-    }
 };
 
 var seatacAirport = {
@@ -13,9 +10,6 @@ var seatacAirport = {
     minHourlyCustomers: 3,
     maxHourlyCustomers: 24,
     averageCookiesPerCustomer: 1.2,
-    randomCustomersPerHour: function() {
-        return Math.ceil(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;
-    }
 };
 
 var seattleCenter = {
@@ -23,9 +17,6 @@ var seattleCenter = {
     minHourlyCustomers: 11,
     maxHourlyCustomers: 38,
     averageCookiesPerCustomer: 3.7, 
-    randomCustomersPerHour: function() {
-        return Math.ceil(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;
-    }
 };
 
 var capitolHill = {
@@ -33,9 +24,6 @@ var capitolHill = {
     minHourlyCustomers: 20,
     maxHourlyCustomers: 38,
     averageCookiesPerCustomer: 2.3, 
-    randomCustomersPerHour: function() {
-        return Math.ceil(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;
-    }
 };
 
 var alki = {
@@ -43,29 +31,34 @@ var alki = {
     minHourlyCustomers: 2,
     maxHourlyCustomers: 16,
     averageCookiesPerCustomer: 4.6, 
-    randomCustomersPerHour: function() {
-        return Math.ceil(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;
-    }
 };
 
-function calculateSimulatedCookiesPerHour(shopLocation){
-    //using military time
-    var openHours = 20 - 6;
+var cookieLocations = [firstPike, seatacAirport, seattleCenter, capitolHill, alki];
+
+function randomCustomersPerHour() {
+    return Math.ceil(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;
+}
+
+cookieLocations.forEach(function(location){
+    location.randomCustomersPerHour = randomCustomersPerHour;
+})
+
+function calculateSimulatedCookiesPerHour(location){
+    var openHours = 14;
     var simulatedCookiesPerHour = [];
     for(var i = 0; i < openHours; i++){
-        simulatedCookiesPerHour[i] = Math.floor(shopLocation.randomCustomersPerHour() * shopLocation.averageCookiesPerCustomer);
+        simulatedCookiesPerHour[i] = Math.floor(location.randomCustomersPerHour() * location.averageCookiesPerCustomer);
     }
     return simulatedCookiesPerHour;
 }
 
-function createStoreLocationHTMLSections(){
-    var cookieLocations = [firstPike, seatacAirport, seattleCenter, capitolHill, alki];
-    for(var i = 0; i < cookieLocations.length; i++){
-        var storeLocation = cookieLocations[i];
-        storeLocation.simulatedCookiesPerHour = calculateSimulatedCookiesPerHour(storeLocation);
-        createH2Element(storeLocation);
-        createULElement(storeLocation);
-        createLIElements(storeLocation);
+function createStoreLocationHTMLSections(locations){
+    for(var i = 0; i < locations.length; i++){
+        var location = locations[i];
+        location.simulatedCookiesPerHour = calculateSimulatedCookiesPerHour(location);
+        createH2Element(location);
+        createULElement(location);
+        createLIElements(location);
     }
 }
 
@@ -106,4 +99,4 @@ function createLIElements(location){
     }
 }
 
-createStoreLocationHTMLSections();
+createStoreLocationHTMLSections(cookieLocations);
