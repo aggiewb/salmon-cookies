aa8e4f83f93454620d3ab49c0d113d0379eb06a4
AggieWB <usmaggie23@gmail.com>
2020-08-02 15:03:09 -0700

    Remove unnecessarily and uninformative comments that came
    with the original code

--- js/form.js
+++ js/form.js
@@ -1,12 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
-    //Get the form elements defined in your form HTML above
     const form = document.querySelector('form');
     const button = document.querySelector('input[type="submit"]');
     const status = document.getElementById('form-submit-status');
 
-    //Success and Error functions for after the form is submitted
     function success() {
         form.reset();
         status.textContent = "Thanks! We'll be getting back to you in 24-48 hours.";
@@ -17,14 +15,12 @@ window.addEventListener('DOMContentLoaded', function() {
         status.textContent = 'Oops! There was a problem.';
     }
 
-    // Handle the form submission event
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         const data = new FormData(form);
         ajax(form.method, form.action, data, success, error);
     });
 
-    //Helper function for sending an AJAX request
     function ajax(method, url, data, success, error) {
         const xhr = new XMLHttpRequest();
         xhr.open(method, url);
